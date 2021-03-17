0748e903b71a0e04d46ef7b71f8322f630512de3
Allahma-Iqbal <abdullah.cse11@gmail.com>
2021-03-17 14:11:11 +0600

    update

--- Decimal to Different bits Binary Recursion/Decimal to Different bits Binary Recursion/main.cpp
+++ Decimal to Different bits Binary Recursion/Decimal to Different bits Binary Recursion/main.cpp
@@ -6,9 +6,60 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define SIZE 100
+int dataArray[SIZE];
+
+
+void printData(int value){
+    for (int i = 0; i<value; i++){
+        cout<<dataArray[i];
+    }
+    cout<<endl;
+}
+
+
+void processingData(int dataArray[], int number, int initvalue){
+    
+    //base case
+    
+    if (initvalue == number){
+        printData(number);
+        return;
+    }else{
+        dataArray[initvalue] = 0;
+        processingData(dataArray, number, initvalue + 1);
+        dataArray[initvalue] = 1;
+        processingData(dataArray, number, initvalue + 1);
+    }
+    
+    
+}
+
+void scanData(){
+    
+    int number;
+    cout<<"Enter a value:";
+    cin>>number;
+    cout<<endl;
+    cout<<"data procesing start..."<<endl;
+    processingData(dataArray, number, 0);
+    
+    
+}
+
+
+
+int main() {
+   
+    
+    
+    while(1){
+        cout << "start scan..."<<endl;
+        scanData();
+        cout<<endl;
+    }
+    
     return 0;
 }
