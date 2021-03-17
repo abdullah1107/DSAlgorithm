022c11574effae9bf3050d2ff93936e94377ee7c
Allahma-Iqbal <abdullah.cse11@gmail.com>
2021-03-17 14:09:42 +0600

    new file add

---
+++ Decimal to Different bits Binary Recursion/Decimal to Different bits Binary Recursion/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Decimal to Different bits Binary Recursion
+//
+//  Created by Muhammad Abdullah Al Mamun on 17/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ SegmentTree/SegmentTree/main.cpp
@@ -0,0 +1,11 @@
+//  main.cpp
+//  SegmentTree
+//  Created by Muhammad Abdullah Al Mamun on 17/3/21.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World"<<endl;
+    return 0;
+}
